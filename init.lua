a2e48f698f72b0c8dca7361a19e6bb22a8fc6d76
Marc Robison <marc@marcrobison.com>
2025-10-27 21:57:59 +0100

    feat: remove save confirmation

--- init.lua
+++ init.lua
@@ -164,7 +164,7 @@ vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+vim.o.confirm = false
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
