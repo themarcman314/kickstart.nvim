23d065afe7b20f21c62574b1c006c79dc68074fd
Marc Robison <marc@marcrobison.com>
2025-11-02 12:12:51 +0100

    refactor: remove spellcheck as default option

--- init.lua
+++ init.lua
@@ -1021,7 +1021,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = '*.tex',
   command = '!latexmk -pdf %',
 })
-vim.opt.spell = true
+--vim.opt.spell = true
 vim.opt.spelllang = { 'fr' }
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
