8e862aa310f7a77c1e67f996463c3d816e42e1ba
Marc Robison <marc@marcrobison.com>
2026-02-09 14:27:47 +0100

    feat: switched nvim-treesitter to stable master branch

--- init.lua
+++ init.lua
@@ -945,6 +945,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master', -- This forces the laptop to use the "old" reliable structure
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
