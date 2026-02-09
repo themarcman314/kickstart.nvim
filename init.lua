195bc3ed7dbf378c6e65fbb00c01f3f235f49326
Marc Robison <marc@marcrobison.com>
2026-02-09 12:39:57 +0100

    feat: add jdtls and terminal binding

--- init.lua
+++ init.lua
@@ -182,6 +182,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<leader>t', ':botright split | terminal<CR>', { desc = 'Open Terminal' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -676,6 +677,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         texlab = {},
+        jdtls = {},
 
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
